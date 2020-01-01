e034bef6de620cce14b4dc30184300cf908d1577
popkovvvv <popkovvvv@mail.ru>
2020-01-01 19:43:51 +0300

    Initial commit

---
+++ src/main/java/com/weTravelTogether/weTravelTogether/WeTravelTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.weTravelTogether.weTravelTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeTravelTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeTravelTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weTravelTogether/weTravelTogether/WeTravelTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weTravelTogether.weTravelTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeTravelTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
