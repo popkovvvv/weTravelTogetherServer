185ce2db8007ef50802d0d0e7cb4faf8c2e508fd
popkovvvv <popkovvvv@mail.ru>
2020-01-01 19:46:59 +0300

    add packages

--- src/main/java/com/weTravelTogether/WeTravelTogetherApplication.java
+++ src/main/java/com/weTravelTogether/WeTravelTogetherApplication.java
@@ -1,4 +1,4 @@
-package com.weTravelTogether.weTravelTogether;
+package com.weTravelTogether;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
