f54ce34f7d787a0600274fa6c845fe011c026d5a
popkovvvv <popkovvvv@mail.ru>
2020-01-04 22:39:17 +0300

    quick fix

--- src/main/java/com/weTravelTogether/models/Account.java
+++ src/main/java/com/weTravelTogether/models/Account.java
@@ -1,10 +1,7 @@
 package com.weTravelTogether.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "account")
